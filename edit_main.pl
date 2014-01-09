15ac08fffec1e1423ce287713674b8b3c8d4d434
Nils <area51.nils@googlemail.com>
2014-01-09 22:55:16 +0100

    first test on real example

--- edit_main.pl
+++ edit_main.pl
@@ -17,11 +17,41 @@ use File::Path qw(make_path);
 
 # ------------------------------------------------------------ #
 # Ordnerstruktur
-my $root = "/home/braun/Dropbox/Projekte/Perl";
-$WAVE::RESULT_DIR = "$root";
+my $ROOT = "/home/braun/HiWi/model-40";
+$WAVE::RESULT_DIR = "$ROOT/test";
+$WAVE::MAGNET_FILE = "$ROOT/Opera-Daten/WAVE-Format/bmap.dat"
+
+my $OFFSET_FILE = "$ROOT/Input/optimierter Offset 8.26.table";
 
 # ------------------------------------------------------------ #
 # Hauptprogramm 
 # 
 
-WAVE::calc();
+open (my $offsetFileHandle, "<", "$OFFSET_FILE") or die "Kann Datei $OFFSET_FILE nicht <C3><B6>ffnen: $!. Abbruch.";
+open (my $resultFileHandle, ">", "$WAVE::RESULT_DIR/result.dat") or die "Konnte $COPY_DIR/result.dat nicht oeffnen. Abbruch.";
+
+# Für jede Energie ausführen
+while(<$offsetFileHandle>)
+{
+		# Energie und Offset auslesen (Achtung: Offset hat noch einen Zeilenumbruch am Ende)
+		# Kommentare überspringen
+		next if ($_ =~ /#.*/ || $_ =~ /^\s*$/);
+
+		(my $energy, my $yStart) = (split /\s+/, $_)[0,1];
+		
+		my $childPID = fork();
+
+		if (! $childPID )
+		{		
+			$SUFFIX = $energy;
+			
+			setValue("XSTART", -0.35);
+			setValue("ISPEC", 1);
+			setValue("IBUNCH", 0);
+			setValue("DMYENERGY", $energy);
+			setValue("YSTART", $yStart/1000.0);
+			setValue("PINCEN(1)", 100);
+			
+			WAVE::calc();
+		}
+}
